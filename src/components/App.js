321cc258c6b7c1d62f9f4122c765471bba58a4a6
Fred <fred.gerner@icloud.com>
2020-10-03 21:40:28 +1000

    fetching categories

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {setCategories} from "../actions";
 
 class App extends Component {
-    componentDidMount(){
-        fetch('http://jservice.io/api/categories?count=20')
-            .then(response => response.json())
-            .then(json => console.log(json));
+    componentDidMount() {
+        if (this.props.categories.length === 0) {
+            fetch('http://jservice.io/api/categories?count=20')
+                .then(response => response.json())
+                .then(json => this.props.setCategories(json));
+        }
     }
+
     render() {
+        console.log('app props', this.props);
         return (<div>
             <h2>Jeopardy!</h2>
         </div>)
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {categories: state}
+}
+
+export default connect(mapStateToProps, {setCategories})(App);
