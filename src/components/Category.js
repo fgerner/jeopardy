8829b8d2269cb0eb955e251b0dcad313c5c9db92
Fred <fred.gerner@icloud.com>
2020-10-07 21:36:49 +1100

    fetching category data

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,12 +4,29 @@ import {connect} from "react-redux";
 
 
 class Category extends Component {
+    constructor() {
+        super ();
+        this.state = {clues: []};
+    }
+    componentDidMount() {
+        fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
+            .then(response => response.json())
+            .then(json => this.setState({clues: json}));
+    }
+
     render() {
         console.log('category', this.props)
         return (
             <div>
                 <Link className={'link-home'} to={'/'}>Home</Link>
-                <h2>Category title</h2>
+                <h2>{this.props.category.title}</h2>
+                {
+                    this.state.clues.map(clue => {
+                        return(
+                            <div key={clue.id}>{clue.question}</div>
+                        )
+                    })
+                }
             </div>
         )
     }
